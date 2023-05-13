64fd142d4458839dc08a360d9b2008391a4f0ff8
DavidWilliamsQA <60790661+DavidWilliamsQA@users.noreply.github.com>
2023-05-13 04:21:02 +0100

    finished assignment

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,22 +1,52 @@
 import { useState } from "react";
 import server from "./server";
+import { utf8ToBytes } from "ethereum-cryptography/utils";
+import { keccak256 } from "ethereum-cryptography/keccak";
+import * as secp from "ethereum-cryptography/secp256k1";
 
-function Transfer({ address, setBalance }) {
+function Transfer({ address, privateKey, setBalance }) {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
+  // const [signature, setSignature] = useState("");
 
   const setValue = (setter) => (evt) => setter(evt.target.value);
+
+  function signMessage(message) {
+    return secp.secp256k1.sign(message, privateKey);
+  }
+
+  function hashMessage(message) {
+    return keccak256(utf8ToBytes(message));
+  }
 
   async function transfer(evt) {
     evt.preventDefault();
 
+    const message = {
+      amount: parseInt(sendAmount),
+      recipient,
+    };
+
+    const messageHash = hashMessage(address);
+
+    const { r, s } = signMessage(messageHash);
+
+    const signature = JSON.stringify({
+      r: r.toString(),
+      s: s.toString(),
+    });
+
     try {
+      console.log(signature);
+      console.log(recipient);
+
       const {
         data: { balance },
       } = await server.post(`send`, {
-        sender: address,
+        signature,
         amount: parseInt(sendAmount),
         recipient,
+        sender: address,
       });
       setBalance(balance);
     } catch (ex) {
--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+const secp = require("ethereum-cryptography/secp256k1");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -7,9 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "0286152c3175b3b9846d346794177e8e839bded28e78028cdf080970e4210386ce": 100,
+  "0331df3f36cb8d79b5217759407a3feaa97aa61f5dcd7be1421e12866ba5651d49": 50,
+  "03606724b2a717ef2961f239f57580f309ca26d778de2c29bc77f15037db192f0f": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,17 +23,37 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  // TODO: get a signature from client-side application
+  // recover the public key from the signature
+  // that will become the senders address
 
-  setInitialBalance(sender);
-  setInitialBalance(recipient);
+  const { recipient, amount, sender, signature } = req.body;
 
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
+  const messageHash = keccak256(utf8ToBytes(sender));
+  const bigSignature = JSON.parse(signature, (key, value) => {
+    if (typeof value === "string") {
+      return BigInt(value);
+    }
+    return value;
+  });
+
+  const isValid = secp.secp256k1.verify(bigSignature, messageHash, sender);
+
+  if (!isValid) {
+    res
+      .status(400)
+      .send({ message: "You are not authorised to make this transaction" });
   } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
+    setInitialBalance(sender);
+    setInitialBalance(recipient);
+
+    if (balances[sender] < amount) {
+      res.status(400).send({ message: "Not enough funds!" });
+    } else {
+      balances[sender] -= amount;
+      balances[recipient] += amount;
+      res.send({ balance: balances[sender] });
+    }
   }
 });
 
